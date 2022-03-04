cb07b7870c228c38498cf39ca0f314848d625c64
Hao Li <80303979+HaoLI-jpg@users.noreply.github.com>
2022-03-04 20:20:13 +1300

    change post layout

--- src/components/Posts/Post/Post.jsx
+++ src/components/Posts/Post/Post.jsx
@@ -1,14 +1,21 @@
 import React from "react";
-import { Card, CardActions, CardContent, CardMedia, Button, Typography } from '@material-ui/core';
+import { Card, CardActions, CardContent, CardMedia, Button, Typography, Box } from '@material-ui/core';
 
 const Post= ( {post} ) => {
     return(
         <Card container>
             <CardMedia component="img" image={post.selectedFile || '../../../../assets/hexagon-structure-3g-1633279.jpeg'} title="Contemplative Reptile"/>
             <CardContent>
-                <Typography variant="h6">{post.title}</Typography>
-                <Typography variant="p6">{post.message}</Typography>
-                <Typography>{post.creator}</Typography>
+                <Typography variant="h5">{post.title}</Typography>
+
+                <Box>
+                    <Typography variant="body1">{post.message}</Typography>
+                </Box>
+
+                <Box display="flex" justifyContent="space-between">
+                    <Typography></Typography>
+                    <Typography gutterBottom variant="body1">{post.creator}</Typography>
+                </Box>
             </CardContent>
         </Card>
     )
